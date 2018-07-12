ee1ab0d8edaf42dc7e54ea9a77bf6ab4435290e5
MinhDAO-Openvalue <minhdung.dao@openvalue.fr>
2018-07-12 15:23:00 +0200

    krakenc >> orders

--- krakenc/main.js
+++ krakenc/main.js
@@ -8,15 +8,48 @@ var argvJson = {}; try {  argvJson = JSON.parse(process.argv[2]);
     	console.log("Display user's balance");
 	    console.log(await krakenc.api('Balance'));
 	    
-	} else if (argvJson.program == 't') {
+	} 
+	
+	else if (argvJson.program == 't') {
 	    console.log(await krakenc.api('Assets', {asset : 'BCH, XBT'}));
 	    
-	} else if (argvJson.program == 'tr') {
+	} 
+	
+	else if (argvJson.program == 'tr') {
       	a = await krakenc.api('Trades', { pair : 'XXBTZEUR', since : 0 });
       	result = a.result
       	console.log(result.last)
       	console.log(result.XXBTZEUR[0])
-    } else {
+      	
+    } 
+    
+       
+    else if (argvJson.program == 'opens') {
+        a = await krakenc.api('OpenOrders', { trades : false});
+        opens = a.result.open
+        openOdersKeys = Object.keys(opens)
+        openOdersKey = openOdersKeys[argvJson.i]
+        console.log(openOdersKey)
+        console.log(opens[openOdersKey])
+            
+    }
+    
+    else if (argvJson.program == 'cancel') {
+        console.log("Cancel an order with txid (transaction id) in parameters : " + argvJson.txid);
+        a = await krakenc.api('CancelOrder', { txid : argvJson.txid});
+            
+    }
+    
+    else if (argvJson.program == 'buy') {
+        console.log("Place an order to buy BCH with limit price and volume in parameters");
+        a = await krakenc.api('AddOrder', { pair: 'BCHEUR', type: 'buy', ordertype: 'limit',
+            price: argvJson.price,
+            volume : argvJson.vol
+        });
+            
+    }
+    
+    else {
 	    console.log("Please enter a program name for example : {\"program\":\"balance\"}")
 	}
 
--- krakenc/readme.txt
+++ krakenc/readme.txt
@@ -2,6 +2,10 @@
 
 npm install
 node main.js {\"program\":\"balance\"}  (without any space)
+node main.js {\"program\":\"opens\",\"i\":1}
+node main.js {\"program\":\"buy\",\"price\":\"510\",\"vol\":\"0.1\"}
+node main.js {\"program\":\"cancel\",\"txid\":\"OAOW2D-E62UU-AVR6XE\"}
+
 node mainCleanOut.js
 node mainGetXLMEUR.js
 
