c655c01146811cdffaa0f765d36a1c22b1a72d70
mdao <minhdung.dao.ext@mappy.com>
2018-07-13 17:07:17 +0200

    krakenc >> xpercent linux

--- krakenc/readme.txt
+++ krakenc/readme.txt
@@ -1,11 +1,11 @@
 -- fill key and secret key in keys.js
 
 npm install
-node main.js {\"program\":\"balance\"}  (without any space)
-node main.js {\"program\":\"opens\",\"i\":1}
-node main.js {\"program\":\"buy\",\"price\":\"510\",\"vol\":\"0.1\"}
-node main.js {\"program\":\"cancel\",\"txid\":\"OAOW2D-E62UU-AVR6XE\"}
-node xpercent.js {\"vol\":0.1,\"x\":10}
+node main.js \{\"program\":\"balance\"\}  (without any space)
+node main.js \{\"program\":\"opens\",\"i\":1\}
+node main.js \{\"program\":\"buy\",\"price\":\"510\",\"vol\":\"0.1\"\}
+node main.js \{\"program\":\"cancel\",\"txid\":\"OAOW2D-E62UU-AVR6XE\"\}
+node xpercent.js \{\"vol\":0.1,\"x\":10\}
 
 
 node mainCleanOut.js
--- krakenc/xpercent.js
+++ krakenc/xpercent.js
@@ -2,20 +2,21 @@ require('./global.js');const krakenc = krakenclient;
 var argvJson = {}; try {  argvJson = JSON.parse(process.argv[2]);
 } catch(error) { console.error("not a valid json argument"); }
 const g = argvJson.x;
-const vol = argvJson.vol
-const volNumber = new Number(vol);
+const volNumber = argvJson.vol;
+
+console.log("do you want to spend " + argvJson.vol + " BCH and gain " + g * volNumber / 100 + " BCH in euro ?")
+console.log("type \"yes i want\" to execute")
+console.log("type \"balance\" to know your balance")
+console.log("type \"price\" to know the open price")
+console.log("type \"cancel\" to cancel two oders")
+console.log("type \"exit\" to exit")
+
 var txid1
 var txid2
-console.log("do you want to spend " + argvJson.vol + " BCH and gain " + g * volNumber / 100 + " BCH in euro ?") 
-console.log("type \"yes i want\" to execute")
-console.log("type \"balance\" to know your balance")  
-console.log("type \"price\" to know the open price")    
-console.log("type \"cancel\" to cancel two oders")   
-console.log("type \"exit\" to exit")     
 var stdin = process.openStdin();
 stdin.addListener("data", function(d) {
-    var entered = d.toString().trim() // note:  d is an object, and when converted to a string it will end with a linefeed.  so we (rather crudely) account for that with toString() and then trim() 
-    console.log("you entered: [" + entered + "]");  
+    var entered = d.toString().trim() // note:  d is an object, and when converted to a string it will end with a linefeed.  so we (rather crudely) account for that with toString() and then trim()
+    console.log("you entered: [" + entered + "]");
     if (entered == "exit") process.exit()
     if (entered == "balance") {
         (async () => {
@@ -40,7 +41,6 @@ stdin.addListener("data", function(d) {
         console.log("Oders are canceled")
     }
     if (entered == "yes i want") {
-        console.log("ok");
         const mf = 0.16/100;
         const a = argvJson.x/2 + 0.16;
         const p1p = (100 - a) / 100;
@@ -49,31 +49,30 @@ stdin.addListener("data", function(d) {
             balance = await krakenc.api('Balance');
             eurobalance = new Number(balance.result.ZEUR)
             console.log("Your balance is : " + eurobalance + "€")
-            
+
             ohlc = await krakenc.api('OHLC', { pair : 'BCHEUR', since : 0 });
             list = ohlc.result.BCHEUR
             nowdata = list[list.length-1]
             p = new Number(nowdata[1])
             console.log("open price is : " + p)
-            
+
             price1 = Math.floor(p1p*p * 10) / 10
             price2 = Math.floor(p2p*p * 10) / 10
-            
+
             oder1 = await krakenc.api('AddOrder', { pair: 'BCHEUR', type: 'buy', ordertype: 'limit',
                 price: price1,
-                volume : vol
+                volume : volNumber
             });
             txid1 = oder1.result.txid[0]
             oder2 = await krakenc.api('AddOrder', { pair: 'BCHEUR', type: 'sell', ordertype: 'limit',
                 price: price2,
-                volume : vol
+                volume : volNumber
             });
             txid2 = oder2.result.txid[0]
+
             console.log("Orders placed : " + price1 + " " + price2)
             console.log("Your future balance would be : " + (eurobalance + g*p*volNumber/100) + "€")
         })();
     }
+    console.log("---------");
 });
-
-    
-
