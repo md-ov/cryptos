42cee315fbc09137768c4a4cd4621cc4b2c5298d
mdao <minhdung.dao.ext@mappy.com>
2018-08-20 17:34:33 +0200

    cryptosbt >> parquet get partitionKeys

--- sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/parquet/Test.scala
+++ sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/parquet/Test.scala
@@ -1,7 +1,7 @@
 package com.minhdd.cryptos.scryptosbt.parquet
 
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{Dataset, SparkSession}
+import com.minhdd.cryptos.scryptosbt.parquet.ParquetFromCSVObj.encoder
+import org.apache.spark.sql.{Dataset, Encoder, SparkSession}
 
 object Test {
     def main(args: Array[String]): Unit = {
@@ -9,18 +9,37 @@ object Test {
         ss.sparkContext.setLogLevel("WARN")
         import ss.implicits._
     
-        val ds1: Dataset[Crypto] = ss.read.parquet("file:///D:\\ws\\cryptos\\data\\parquets\\parquet1").as[Crypto]
-        val ds2: Dataset[Crypto] = ss.read.parquet("file:///D:\\ws\\cryptos\\data\\parquets\\parquet2").as[Crypto]
+        val ds1: Dataset[Crypto] = ss.read.parquet("file:///home/mdao/minh/git/cryptos/data/parquets/parquet").as[Crypto]
+        val ds2: Dataset[Crypto] = ss.read.parquet("file:///home/mdao/minh/git/cryptos/data/parquets/parquet").as[Crypto]
+//        val ds2: Dataset[Crypto] = ss.read.parquet("file:///D:\\ws\\cryptos\\data\\parquets\\parquet2").as[Crypto]
         
-        
+//        val map = toPartitions(ds1, ds2)
 //        val ds11: RDD[(CryptoPartitionKey, Crypto)] = ds1.toJavaRDD.rdd.map(c => ((c.partitionKey), c))
         
-        
+        val keys = getPartitionKeys(ss, ds1)
+        println(keys)
         
         println(ds1.count())
         println(ds2.count())
         val ds = ds1.union(ds2)
         println(ds.count())
         ds.toDF().show(false)
+    }
+
+    def encoder(ss: SparkSession): Encoder[Seq[CryptoPartitionKey]] = {
+        import ss.implicits._
+        implicitly[Encoder[Seq[CryptoPartitionKey]]]
+    }
+    
+    def reduceSeq(seq1: Seq[CryptoPartitionKey], seq2: Seq[CryptoPartitionKey]): Seq[CryptoPartitionKey] = {
+        seq2.filter(c => !seq1.exists(c.equals(_))) ++ seq1
+    }
+
+    def getPartitionKeys(ss: SparkSession, ds: Dataset[Crypto]): Seq[CryptoPartitionKey] = {
+        ds.map(c => Seq(c.partitionKey))(encoder(ss)).reduce((seq1, seq2) => reduceSeq(seq1, seq2))
+    }
+    
+    def toPartitions(ds1: Dataset[Crypto], ds2: Dataset[Crypto]): Map[CryptoPartitionKey, Dataset[Crypto]] = {
+          ???
     }
 }
