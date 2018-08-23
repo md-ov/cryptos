501c09e58e558ac90252c07b9a8ff9ba5e1a9463
MinhDAO-Openvalue <minhdung.dao@openvalue.fr>
2018-08-23 11:01:56 +0200

    undo commit keys

--- krakenc/keys.js
+++ krakenc/keys.js
@@ -1,3 +1,2 @@
-exports.key       = "vd6Fmxptmh+RiOo60vu88gBwFNKmnLFsu77NmbB046PBFUsHLeEj/Bak"; // API Key
-exports.secret    = "kyp19P3khsj1bPNC0FNyObx5yjKUUVZRDQSZgSUe0ZPGVrSsTtEgRGKBhBe6AZlf9tBfIJxZZsKIy/rpNZZduw=="; // API Private Key
-
+exports.key       = ""; // API Key
+exports.secret    = ""; // API Private Key
