d8d2e45dac2f6d0024816c6f1581f604f0f9cef7
MinhDAO-Openvalue <minhdung.dao@openvalue.fr>
2018-08-23 10:59:33 +0200

    gitignore

--- krakenc/keys.js
+++ krakenc/keys.js
@@ -1,3 +1,3 @@
-exports.key       = ""; // API Key
-exports.secret    = ""; // API Private Key
+exports.key       = "vd6Fmxptmh+RiOo60vu88gBwFNKmnLFsu77NmbB046PBFUsHLeEj/Bak"; // API Key
+exports.secret    = "kyp19P3khsj1bPNC0FNyObx5yjKUUVZRDQSZgSUe0ZPGVrSsTtEgRGKBhBe6AZlf9tBfIJxZZsKIy/rpNZZduw=="; // API Private Key
 
--- krakenc/mainCleanOut.js
+++ krakenc/mainCleanOut.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 
 fs.unlink("./out/xlmeur.csv", function (err) {
   if (err) throw err;
-  console.log('successfully deleted out/*.csv');
+  console.log('successfully deleted out/xlmeur.csv');
 });
