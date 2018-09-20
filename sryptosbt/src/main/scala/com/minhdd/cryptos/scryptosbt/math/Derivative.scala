da278386a1ae282a0f7195ed42e1c0f1a41016a9
MinhDAO-Openvalue <minhdung.dao@openvalue.fr>
2018-09-20 17:19:44 +0200

    cryptosbt >> derivative

---
+++ sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/math/Derivative.scala
@@ -0,0 +1,82 @@
+package com.minhdd.cryptos.scryptosbt.math
+
+import org.apache.spark.sql.{Dataset, Encoder, SparkSession}
+
+object Derivative {
+    def derive(i: Seq[Double], j: Seq[Double]): Seq[Double] = {
+        i.indices.map(indice => {
+            val x = i.apply(indice)
+            val y = j.apply(indice)
+            val previousX = if (indice == 0) x else i.apply(indice - 1)
+            val previousY = if (indice == 0) y else j.apply(indice - 1)
+            val nextX = if (indice == i.length - 1) x else i.apply(indice + 1)
+            val nextY = if (indice == i.length - 1) y else j.apply(indice + 1)
+            if (indice == 0) ((nextY - y) / (nextX - x)) - (nextX - x)
+            else if (indice == i.length - 1) ( (y - previousY) / (x - previousX) ) + (x - previousX)
+            else (nextY - previousY) / (nextX - previousX)
+        })
+    }
+}
+
+object DerivativeDatasetDoubleDouble {
+    type DD = (Double, Double)
+    def toX(dd: DD) = dd._1
+    def toY(dd: DD) = dd._2
+    def encoder(ss: SparkSession): Encoder[(DD, Double)] = {
+        import ss.implicits._
+        implicitly[Encoder[(DD, Double)]]
+    }
+    
+    def derive(d: Dataset[DD], ss: SparkSession): Dataset[(DD, Double)] =
+        d.mapPartitions(iterator => new Iterator[(DD, Double)]{
+            var previous: Option[DD] = None
+            var actual: Option[DD] = if (iterator.hasNext) Some(iterator.next) else None
+            override def hasNext: Boolean = actual.isDefined
+            override def next(): (DD, Double) = {
+                val actualGet: DD = actual.get
+                val nextt: Option[DD] = if (iterator.hasNext) Some(iterator.next) else None
+                val derived: Double =
+                    if (previous.isEmpty) {
+                        if (nextt.isEmpty) 1D else {
+                            val nextGet = nextt.get
+                            previous = actual
+                            actual = nextt
+                            (toY(nextGet) - toY(actualGet))/(toX(nextGet) - toX(actualGet)) - ( toX(nextGet) - toX(actualGet) )
+                        }
+                    } else if (nextt.isEmpty) {
+                        val previousGet = previous.get
+                        previous = actual
+                        actual = nextt
+                        (toY(actualGet) - toY(previousGet))/(toX(actualGet) - toX(previousGet)) + (toX(actualGet) - toX(previousGet))
+                    } else {
+                        val nextGet = nextt.get
+                        val previousGet = previous.get
+                        previous = actual
+                        actual = nextt
+                        (toY(nextGet) - toY(previousGet)) / (toX(nextGet) - toX(previousGet))
+                    }
+                
+                return (actualGet, derived)
+            }
+        })(encoder(ss))
+    
+    def derive2(d: Dataset[DD], ss: SparkSession): Dataset[(DD, Double)] = {
+        d.mapPartitions(p => {
+            val v: Seq[DD] = p.toVector
+            val r: Seq[(DD, Double)] = v.indices.map(indice => {
+                val actual = v.apply(indice)
+                val x: Double = toX(actual)
+                val y: Double = toY(actual)
+                val previousX = if (indice == 0) x else toX(v.apply(indice - 1))
+                val previousY = if (indice == 0) y else toY(v.apply(indice - 1))
+                val nextX = if (indice == v.length - 1) x else toX(v.apply(indice + 1))
+                val nextY = if (indice == v.length - 1) y else toY(v.apply(indice + 1))
+                val derived: Double = if (indice == 0) (nextY - y) / (nextX - x) - (nextX - x)
+                else if (indice == v.length - 1) (y - previousY) / (x - previousX) + (x - previousX)
+                else (nextY - previousY) / (nextX - previousX)
+                (v.apply(indice), derived)
+            })
+            r.toIterator
+        })(encoder(ss))
+    }
+}
---
+++ sryptosbt/src/test/scala/com/minhdd/cryptos/scryptosbt/math/DerivativeTest.scala
@@ -0,0 +1,46 @@
+package com.minhdd.cryptos.scryptosbt.math
+
+import org.apache.spark.sql.{Dataset, SparkSession}
+import org.scalatest.FunSuite
+
+class DerivativeTest extends FunSuite {
+    
+    test("test derivative seq") {
+        val x = (-20 until 20).map(_.toDouble)
+        val y = x.map(d => d * d + d)
+        val expected: Seq[Double] = x.map(d => 2 * d + 1)
+        val derived: Seq[Double] = Derivative.derive(x, y)
+        println(expected)
+        println(derived)
+        assert(derived == expected)
+    }
+    
+    test("test derivative 2 dataset") {
+        val ss: SparkSession = SparkSession.builder().appName("derivation").master("local[*]").getOrCreate()
+        ss.sparkContext.setLogLevel("WARN")
+        
+        val s: Seq[(Double, Double)] = (-20 until 20).map(_.toDouble).map(d => (d, d * d + d))
+        val expected: Seq[Double] = s.map(d => 2 * d._1 + 1)
+        println(expected)
+        import ss.implicits._
+        val ds: Dataset[(Double, Double)] = ss.createDataset(s)
+        val derived: Seq[Double] = DerivativeDatasetDoubleDouble.derive2(ds, ss).map(_._2).collect().toSeq
+        println(derived)
+        assert(derived == expected)
+    }
+    
+    test("test derivative dataset") {
+        val ss: SparkSession = SparkSession.builder().appName("derivation").master("local[*]").getOrCreate()
+        ss.sparkContext.setLogLevel("WARN")
+        
+        val s: Seq[(Double, Double)] = (-20 until 20).map(_.toDouble).map(d => (d, d * d + d))
+        val expected: Seq[Double] = s.map(d => 2 * d._1 + 1)
+        println(expected)
+        import ss.implicits._
+        val ds: Dataset[(Double, Double)] = ss.createDataset(s)
+        val derived: Seq[Double] = DerivativeDatasetDoubleDouble.derive(ds.coalesce(1), ss).map(_._2).collect().toSeq
+        println(derived)
+        assert(derived == expected)
+    }
+    
+}
