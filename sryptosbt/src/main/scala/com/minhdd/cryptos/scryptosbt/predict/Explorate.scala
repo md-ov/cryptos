97d360067efee46f3a5b6ef6cae28af585a31647
MinhDAO-Openvalue <minhdung.dao@openvalue.fr>
2018-10-11 18:22:46 +0200

    cryptosbt >> numberOfStableDay

--- sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/predict/Explorate.scala
+++ sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/predict/Explorate.scala
@@ -2,14 +2,15 @@ package com.minhdd.cryptos.scryptosbt.predict
 
 import com.minhdd.cryptos.scryptosbt.parquet.{Crypto, CryptoPartitionKey}
 import com.minhdd.cryptos.scryptosbt.tools.Timestamps
-
-import org.apache.spark.sql.{Dataset, SparkSession}
+import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Column, Dataset, Row, SparkSession}
 
 object Explorate {
     
     val maximumDeltaTime = 4 * Timestamps.oneDayTimestampDelta
     val minDeltaValue = 350
-    
+
     
     def main(args: Array[String]): Unit = {
         val ss: SparkSession = SparkSession.builder().appName("explorate").master("local[*]").getOrCreate()
@@ -36,19 +37,59 @@ object Explorate {
     
         import org.apache.spark.sql.functions.{max, min, col, when}
     
-        analyticsCryptoDS
+        val evolutionColumnName = "evolution"
+        val evolutionNullValue = "-"
+        val aaa = analyticsCryptoDS
           .withColumn("value", col(cryptoValueColumnName))
           .withColumn("max", max("value").over(window))
           .withColumn("min", min("value").over(window))
           .withColumn("variation",
               max(cryptoValueColumnName).over(window) - min(cryptoValueColumnName).over(window))
-          .select(datetimeColumnName, "value", "max", "min", "variation", "analytics.derive")
-          .withColumn("evolution", 
+          .withColumn(evolutionColumnName, 
               when($"min" === $"value" && $"variation" > minDeltaValue, "down")
                 .when($"max" === $"value" && $"variation" > minDeltaValue, "up")
-                .otherwise("-"))
+                .otherwise(evolutionNullValue))
 //          .filter("evolution != '-'")
 //          .filter(($"evolution" === "up" && $"derive" < 0) || ($"evolution" === "down" && $"derive" > 0))
+//          .select(datetimeColumnName, "value", "variation", "evolution", "analytics.derive")
+//          .show(1000, false)
+    
+        val w = Window.orderBy(datetimeColumnName)
+        
+        val binaryEvolution = when($"evolution" === evolutionNullValue, 1).otherwise(0)
+        val persistenceColumnName = "persistence"
+        val customSum = new CustomSum()
+        val persistence: Column = customSum(binaryEvolution).over(w)
+
+        val ccc = aaa.withColumn(persistenceColumnName, persistence)
+            ccc.select(datetimeColumnName, "value", "variation", evolutionColumnName, "analytics.derive", persistenceColumnName)
           .show(1000, false)
+    
+    
     }
-}
+    
+    class CustomSum extends UserDefinedAggregateFunction {
+        override def inputSchema: org.apache.spark.sql.types.StructType =
+            StructType(StructField("value", DoubleType) :: Nil)
+        
+        override def bufferSchema: StructType = StructType(
+            StructField("value", LongType) :: Nil
+        )
+        override def dataType: DataType = LongType
+        override def deterministic: Boolean = true
+        override def initialize(buffer: MutableAggregationBuffer): Unit = {
+            buffer(0) = 0L
+        }
+        override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
+            val value2 = input.getAs[Double](0)
+            if (value2 == 0) buffer(0) = 0L else buffer(0) = buffer.getAs[Long](0) + 1
+        }
+        override def merge(buffer: MutableAggregationBuffer, buffer2: Row): Unit = {
+            println("merge buffer " + buffer(0))
+            println("merge row" + buffer.getAs[Long](0))
+        }
+        override def evaluate(buffer: Row): Any = {
+            buffer.getLong(0)
+        }
+    }
+   }
