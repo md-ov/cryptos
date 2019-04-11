12270605cc03d7f8fe3b0374ee387d89ba0e5ce2
MinhDAO-Openvalue <minhdung.dao@openvalue.fr>
2019-04-11 16:10:30 +0200

    cryptosbt >> last regresser

--- sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/predict/ml/MLSegmentsGBTRegressor.scala
+++ sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/predict/ml/MLSegmentsGBTRegressor.scala
@@ -1,13 +1,16 @@
 package com.minhdd.cryptos.scryptosbt.predict.ml
 
+import java.sql.Timestamp
+
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.evaluation.RegressionEvaluator
-import org.apache.spark.ml.feature.{Binarizer, StringIndexer}
+import org.apache.spark.ml.feature.Binarizer
 import org.apache.spark.ml.regression.GBTRegressor
 import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import com.minhdd.cryptos.scryptosbt.predict.ml.ml._
+import com.minhdd.cryptos.scryptosbt.tools.Timestamps
 
 object MLSegmentsGBTRegressor {
     
@@ -18,18 +21,21 @@ object MLSegmentsGBTRegressor {
     val predict = "predict"
     
     def main(args: Array[String]): Unit = {
-        val evaluator = new RegressionEvaluator().setLabelCol("label").setPredictionCol("prediction")
+        val ts1 = Timestamps.now
+        println("begin : "  + ts1)
         
+        val evaluator = new RegressionEvaluator().setLabelCol("label").setPredictionCol("prediction")
         val gbt = new GBTRegressor()
         gbt.setSeed(273).setMaxIter(5)
         val ml = gbt
         val paramGrid = new ParamGridBuilder().addGrid(gbt.maxIter, Array(5, 10, 20, 50, 100)).build()
-        val pipeline = new Pipeline().setStages(Array(indexerBegin, vectorAssembler, ml))
+        val pipelineForAll = new Pipeline().setStages(Array(indexerBegin, vectorAssembler, ml))
+    
+        val cvForAll = new CrossValidator()
+          .setEstimator(pipelineForAll).setEvaluator(evaluator).setEstimatorParamMaps(paramGrid)
+          .setNumFolds(3).setSeed(27)
+        
         val pipelineForTrueSegment = new Pipeline().setStages(Array(indexerBegin, indexerEnd, vectorAssembler, ml))
-
-        val cv = new CrossValidator()
-            .setEstimator(pipeline).setEvaluator(evaluator).setEstimatorParamMaps(paramGrid)
-            .setNumFolds(3).setSeed(27)
         
         val cvForTrueSegment = new CrossValidator()
           .setEstimator(pipelineForTrueSegment).setEvaluator(evaluator).setEstimatorParamMaps(paramGrid)
@@ -41,45 +47,65 @@ object MLSegmentsGBTRegressor {
         val dfWithoutBeginEvolutionNull: DataFrame =
             ss.read
               .option("sep", ";").schema(csvSchema)
-              .csv("D:\\ws\\cryptos\\data\\csv\\segments\\all-190221")
+              .csv("D:\\ws\\cryptos\\data\\csv\\segments\\all-190407")
               .filter(!(col("begin-evolution") === "-"))
               
         val dfForTrueSegment = dfWithoutBeginEvolutionNull.filter(!(col("end-evolution") === "-"))
         val modelForTrueSegment = cvForTrueSegment.fit(dfForTrueSegment)      
               
-        val df = dfWithoutBeginEvolutionNull.withColumn("label",
+        ///// begin of segment detection 
+        
+        val dfOfAll = dfWithoutBeginEvolutionNull.withColumn("label",
                     when(col("end-evolution") === "up", 1)
-                    .when(col("end-evolution") === "down", -1)
-                    .otherwise(0))
+                    .when(col("end-evolution") === "down", 0)
+                    .otherwise(-1))
     
-        val Array(trainDF, testDF) = df.randomSplit(Array(0.7, 0.3), seed=42)
-        val model = cv.fit(trainDF)
-        val resultDF: DataFrame = 
-            model.transform(testDF)
+        val Array(trainDF, testDF) = dfOfAll.randomSplit(Array(0.7, 0.3), seed=42)
+        val modelOFSegmentDetection = cvForAll.fit(trainDF)
+        val segmentDetectionResult: DataFrame = 
+            modelOFSegmentDetection.transform(testDF)
               .withColumn(absolute_predict, abs(col(prediction)))
               .withColumn(absolute_label, abs(col(label)))
     
-        val binarizer = new Binarizer()
-          .setInputCol(absolute_predict)
+        segmentDetectionResult.select("begin-evolution", "end-evolution", "label", "prediction")
+          .write.csv("D:\\ws\\cryptos\\data\\mlresults\\all-190407")
+        
+        val binarizerForSegmentDetection = new Binarizer()
+          .setInputCol(prediction)
           .setOutputCol(predict)
-          .setThreshold(0.9)
-        
-        val binaryResultDf = binarizer.transform(resultDF)
-        
-        val filtered = binaryResultDf.filter(col(predict) === 1)
-          .drop(label, predict, prediction, "begin-evo", "features")
-          .filter(!(col("begin-evolution") === "-"))
-        
-        val finalResultDF = modelForTrueSegment.transform(filtered)
-        finalResultDF.show(false)
+          .setThreshold(0.5)
+
+        val segmentDetectionBinaryResults = binarizerForSegmentDetection.transform(segmentDetectionResult)
     
-        val finalBinarizer = new Binarizer()
-            .setInputCol("prediction")
-            .setOutputCol("predict")
-            .setThreshold(0.5)
+        println("segmentDetectionBinaryResults : ")
+        segmentDetectionBinaryResults.show(false)
+    
+        segmentDetectionBinaryResults.groupBy("label", predict).count().show()
         
-        val finalBinarizedResult = finalBinarizer.transform(finalResultDF)
-        finalBinarizedResult.groupBy(label, predict).count().show(false)
+//        val detectedSegments = segmentDetectionBinaryResults.filter(col(predict) === 1)
+//          .drop(label, predict, prediction, "begin-evo", "features")
+//          .filter(!(col("begin-evolution") === "-"))
+//        
+//        val finalResults: DataFrame = modelForTrueSegment.transform(detectedSegments)
+//    
+//        println("final results : ")
+//        finalResults.show(false)
+//        
+//        finalResults.select("begin-evolution", "end-evolution", "label", "prediction")
+//          .write.csv("D:\\ws\\cryptos\\data\\mlresults\\5.txt")
+//        
+//        val finalBinarizer = new Binarizer()
+//            .setInputCol("prediction")
+//            .setOutputCol("predict")
+//            .setThreshold(0.5)
+//        
+//        val finalBinaryResults: DataFrame = finalBinarizer.transform(finalResults)
+//        finalBinaryResults.groupBy(label, predict).count().show(false)
+    
+    
+        val ts2: Timestamp = Timestamps.now
+        println("fin : " + ts2)
+        println(ts2.getTime - ts1.getTime)
     }
 }
 
