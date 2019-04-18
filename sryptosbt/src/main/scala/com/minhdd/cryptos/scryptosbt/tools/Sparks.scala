d0e301f4f99c131aa4babb9d0258b4b30d72bd94
MinhDAO-Openvalue <minhdung.dao@openvalue.fr>
2019-04-18 17:24:46 +0200

    cryptosbt >> better write

--- sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/tools/Sparks.scala
+++ sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/tools/Sparks.scala
@@ -2,11 +2,16 @@ package com.minhdd.cryptos.scryptosbt.tools
 
 import com.minhdd.cryptos.scryptosbt.parquet.CSVFromParquetObj.merge
 import com.minhdd.cryptos.scryptosbt.parquet.Crypto
+import com.minhdd.cryptos.scryptosbt.predict.BeforeSplit
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 object Sparks {
-    def csvFromDSString(dsString: Dataset[String], csvPath: String) = {
-        dsString.coalesce(1)
+    
+    def parquetFromDs(ds: Dataset[_], parquetPath: String) = {
+        ds.write.parquet(parquetPath)
+    }
+    def csvFromDS(ds: Dataset[_], csvPath: String) = {
+        ds.coalesce(1)
           .write.format("com.databricks.spark.csv")
           .save(csvPath)
         merge(csvPath, csvPath + ".csv")
@@ -15,17 +20,18 @@ object Sparks {
     def csvFromDSCrypto(ss: SparkSession, csvPath: String, ds: Dataset[Crypto]) = {
         import ss.implicits._
         val dsString: Dataset[String] = ds.map(_.flatten.toLine())
-        csvFromDSString(dsString, csvPath)
+        csvFromDS(dsString, csvPath)
     }
     
     def csvFromSeqCrypto(ss: SparkSession, csvPath: String, seq: Seq[Crypto]) = {
         val ds: Dataset[Crypto] = ss.createDataset(seq)(Crypto.encoder(ss))
         import ss.implicits._
         val dsString: Dataset[String] = ds.map(_.flatten.toLine())
-        csvFromDSString(dsString, csvPath)
+        csvFromDS(dsString, csvPath)
     }
     
     def csvFromDataframe(csvPath: String, df: DataFrame) = {
         df.coalesce(1).write.option("delimiter", ";").csv(csvPath)
     }
+
 }
