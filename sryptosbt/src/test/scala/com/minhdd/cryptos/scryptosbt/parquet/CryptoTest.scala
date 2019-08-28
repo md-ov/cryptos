9d18c6d8a9dc4066c73a9b190d941be71e16ba4e
minhov <minhdung.dao@openvalue..fr>
2019-08-28 11:30:44 +0200

    cryptosbt >> nothing special

--- sryptosbt/src/test/scala/com/minhdd/cryptos/scryptosbt/parquet/CryptoTest.scala
+++ sryptosbt/src/test/scala/com/minhdd/cryptos/scryptosbt/parquet/CryptoTest.scala
@@ -12,7 +12,7 @@ class CryptoTest extends FunSuite {
     test("testParseLine") {
         val line = "XLM;EUR;kraken;ohlc;2018-04-16T06:00:00GMT+0200;1523894400;0.226500;0.229800;0.220821;0.225699;" +
           "0.226591;1087016.31536331;584"
-        val parsed = Crypto.parseOHLC(line).apply(0)
+        val parsed: Crypto = Crypto.parseOHLC(line).apply(0)
         assert(parsed.cryptoValue == CryptoValue(
             value = 0.2265,
             volume = 1087016.31536331,
