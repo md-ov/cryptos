4448814bd11807b3e642864338761e89c69d1bac
minhov <minhdung.dao@openvalue..fr>
2019-10-16 16:47:36 +0200

    cryptosbt >> split ok, dont use last seq which is not a segment

--- sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/service/Splitter.scala
+++ sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/service/Splitter.scala
@@ -7,9 +7,7 @@ object Splitter {
     
     def toBigSegments(beforeSplits: Seq[BeforeSplit]): Seq[Seq[BeforeSplit]] = {
         val zipped: Seq[(BeforeSplit, Int)] = beforeSplits.zipWithIndex
-        val splitPositions: Seq[Int] = zipped.filter(x =>
-            toSplit(x._1) || x._2 == beforeSplits.length-1
-        ).map(_._2)
+        val splitPositions: Seq[Int] = zipped.filter(x => toSplit(x._1)).map(_._2)
         val seqq: Seq[(Int, Int)] = splitPositions.foldLeft((0, Seq.empty[(Int,Int)])){
             (acc, newP) => (newP, acc._2 ++ Seq((acc._1, newP)))
         }._2
