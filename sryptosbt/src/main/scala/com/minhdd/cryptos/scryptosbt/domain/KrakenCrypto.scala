99c19a63969366b56cd68202c1a68e6c7339e15f
minhov <minhdung.dao@openvalue..fr>
2019-10-23 18:35:41 +0200

    cryptosbt >> refactoring

--- sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/domain/KrakenCrypto.scala
+++ sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/domain/KrakenCrypto.scala
@@ -5,15 +5,16 @@ import java.sql.Timestamp
 import org.apache.spark.sql.{Encoder, SparkSession}
 
 case class KrakenCrypto(
-    datetime: Timestamp,
-    value: Double,
-    volume: Double,
-    count: Option[Int],
-    ohlcValue: Option[Double],
-    ohlcVolume: Option[Double]
-)
+                         datetime: Timestamp,
+                         value: Double,
+                         volume: Double,
+                         count: Option[Int],
+                         ohlcValue: Option[Double],
+                         ohlcVolume: Option[Double]
+                       )
 
 object KrakenCrypto {
+    
     def encoder(spark: SparkSession): Encoder[KrakenCrypto] = {
         import spark.implicits._
         implicitly[Encoder[KrakenCrypto]]
--- sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/service/segment/SpacingSpreadingJoiner.scala
+++ sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/service/segment/SpacingSpreadingJoiner.scala
@@ -4,6 +4,7 @@ import java.sql.Timestamp
 
 import com.minhdd.cryptos.scryptosbt.constants.numberOfMinutesBetweenTwoElement
 import com.minhdd.cryptos.scryptosbt.domain.{Crypto, CryptoPartitionKey, CryptoValue, KrakenCrypto}
+import com.minhdd.cryptos.scryptosbt.service.segment.CryptoContainer.SeqCryptoContainerImplicit
 import com.minhdd.cryptos.scryptosbt.tools.DateTimeHelper
 import com.minhdd.cryptos.scryptosbt.tools.TimestampHelper.TimestampImplicit
 import org.apache.spark.sql.expressions.Window
@@ -37,10 +38,40 @@ object CryptoContainer {
         implicitly[Encoder[CryptoContainer]]
     }
     
-    def encoderForGroup(spark: SparkSession): Encoder[(Timestamp, Seq[CryptoContainer])] = {
+    def groupByEncoder(spark: SparkSession): Encoder[(Timestamp, Seq[CryptoContainer])] = {
         import spark.implicits._
         implicitly[Encoder[(Timestamp, Seq[CryptoContainer])]]
     }
+    
+    def fromOHLCToCryptoContainer(timestamp: Timestamp, ohlc: CryptoContainer): KrakenCrypto = {
+        KrakenCrypto(
+            datetime = timestamp,
+            value = ohlc.cryptoValue.value,
+            volume = ohlc.cryptoValue.volume,
+            count = ohlc.count,
+            ohlcValue = Some(ohlc.cryptoValue.value),
+            ohlcVolume = Some(ohlc.cryptoValue.volume)
+        )
+    }
+    
+    def fromTRADESToCryptoContainer(timestamp: Timestamp, trades: CryptoContainer): KrakenCrypto = {
+        KrakenCrypto(
+            datetime = timestamp,
+            value = trades.cryptoValue.value,
+            volume = trades.cryptoValue.volume,
+            count = trades.count,
+            ohlcValue = None,
+            ohlcVolume = None
+        )
+    }
+    
+    implicit class SeqCryptoContainerImplicit(seq: Seq[CryptoContainer]) {
+        def middleCryptoValue: CryptoValue = {
+            val length = seq.length
+            seq.map(_.cryptoValue).sortWith(_.value > _.value).apply(length / 2)
+        }
+    }
+    
 }
 
 object CryptoSpreadContainer {
@@ -52,128 +83,44 @@ object CryptoSpreadContainer {
 
 object SpacingSpreadingJoiner {
     
-    def oneKrakenCrypto(timestamp: Timestamp, containersTrades: Seq[CryptoContainer], containersOhlcs: Seq[CryptoContainer]): KrakenCrypto = {
-        
-        if (containersTrades.isEmpty && containersOhlcs.length == 1) {
-            val firstOhlc = containersOhlcs.head
-            KrakenCrypto(
-                datetime = timestamp,
-                value = firstOhlc.cryptoValue.value,
-                volume = firstOhlc.cryptoValue.volume,
-                count = firstOhlc.count,
-                ohlcValue = Some(firstOhlc.cryptoValue.value),
-                ohlcVolume = Some(firstOhlc.cryptoValue.volume)
-            )
-        } else if (containersTrades.length == 1 && containersOhlcs.isEmpty) {
-            val firstTrades = containersTrades.head
-            KrakenCrypto(
-                datetime = timestamp,
-                value = firstTrades.cryptoValue.value,
-                volume = firstTrades.cryptoValue.volume,
-                count = firstTrades.count,
-                ohlcValue = None,
-                ohlcVolume = None
-            )
-        } else if (containersTrades.isEmpty) {
-            val volumeTotal: Double = containersOhlcs.map(_.cryptoValue.volume).sum
-            val length = containersOhlcs.length
-            val countSum: Long = containersOhlcs.map(_.count.getOrElse(0)).sum
-            val countAverage = (countSum / length).toInt
-            val cryptoValue: CryptoValue = middleCryptoValue(containersOhlcs, length)
-            KrakenCrypto(
-                datetime = timestamp,
-                value = cryptoValue.value,
-                volume = volumeTotal,
-                count = Option(countAverage),
-                ohlcValue = Option(cryptoValue.value),
-                ohlcVolume = Option(volumeTotal)
-            )
-        } else if (containersOhlcs.isEmpty) {
-            val length = containersTrades.length
-            val volumeTotal: Double = containersTrades.map(_.cryptoValue.volume).sum
-            val cryptoValue: CryptoValue = middleCryptoValue(containersTrades, length)
-            KrakenCrypto(
-                datetime = timestamp,
-                value = cryptoValue.value,
-                volume = volumeTotal,
-                count = None,
-                ohlcValue = None,
-                ohlcVolume = None
-            )
-        } else {
-            val lengthOhlc = containersOhlcs.length
-            val lengthTrades = containersTrades.length
-            val countSum: Long = containersOhlcs.map(_.count.getOrElse(0)).sum
-            val countAverage = (countSum / lengthOhlc).toInt
-            val volumeOhlcTotal: Double = containersOhlcs.map(_.cryptoValue.volume).sum
-            val volumeTotal: Double = containersTrades.map(_.cryptoValue.volume).sum + volumeOhlcTotal
-            val cryptoValue: CryptoValue = middleCryptoValue(containersTrades ++ containersOhlcs, lengthOhlc + lengthTrades)
-            val ohlcCryptoValue = middleCryptoValue(containersOhlcs, lengthOhlc)
-            KrakenCrypto(
-                datetime = timestamp,
-                value = cryptoValue.value,
-                volume = volumeTotal,
-                count = Option(countAverage),
-                ohlcValue = Option(ohlcCryptoValue.value),
-                ohlcVolume = Option(volumeTotal)
-            )
-        }
+    def join(spark: SparkSession, trades: Dataset[Crypto], ohlcs: Dataset[Crypto]): Dataset[KrakenCrypto] = {
+        spread(space(trades, ohlcs))
     }
     
-    private def middleCryptoValue(seq: Seq[CryptoContainer], length: Int): CryptoValue = {
-        seq.map(_.cryptoValue).sortWith(_.value > _.value).apply(length / 2)
-    }
-    
-    def fill(cryptoSpreadContainer: CryptoSpreadContainer, numberOfMinutesBetweenTwoElement: Int): Seq[KrakenCrypto] = {
-        if (cryptoSpreadContainer.next == null) Seq(cryptoSpreadContainer.toKrakenCrypto) else {
-            val tss: Seq[Timestamp] =
-                DateTimeHelper.getTimestamps(
-                    cryptoSpreadContainer.timestamp,
-                    cryptoSpreadContainer.next,
-                    numberOfMinutesBetweenTwoElement)
-            tss.map(ts => cryptoSpreadContainer.copy(timestamp = ts).toKrakenCrypto)
-        }
-    }
-    
-    def spread(spark: SparkSession, spacedKrakenCryptos: Dataset[KrakenCrypto]): Dataset[KrakenCrypto] = {
+    private def spread(spacedKrakenCryptos: Dataset[KrakenCrypto]): Dataset[KrakenCrypto] = {
         val window = Window.orderBy("datetime")
-        val cryptoSpreadContainers: Dataset[CryptoSpreadContainer] =
+        implicit val encoder: Encoder[KrakenCrypto] = KrakenCrypto.encoder(spacedKrakenCryptos.sparkSession)
+        implicit val groupbyEncoder: Encoder[CryptoSpreadContainer] = CryptoSpreadContainer.encoder(spacedKrakenCryptos.sparkSession)
+        
+        val ds: Dataset[CryptoSpreadContainer] =
             spacedKrakenCryptos
               .withColumn("timestamp", col("datetime"))
               .withColumn("next", lead("datetime", 1).over(window))
-              .as[CryptoSpreadContainer](CryptoSpreadContainer.encoder(spark))
+              .as[CryptoSpreadContainer]
         
-        cryptoSpreadContainers.flatMap(fill(_, numberOfMinutesBetweenTwoElement))(KrakenCrypto.encoder(spark))
+        ds.flatMap(fill)
     }
     
-    def join(spark: SparkSession, trades: Dataset[Crypto], ohlcs: Dataset[Crypto]): Dataset[KrakenCrypto] = {
-        val spacedKrakenCryptos: Dataset[KrakenCrypto] = space(spark, trades, ohlcs)
-        spread(spark, spacedKrakenCryptos)
-    }
-    
-    def space(spark: SparkSession, trades: Dataset[Crypto], ohlcs: Dataset[Crypto]): Dataset[KrakenCrypto] = {
-        
-        def toContainers(cryptos: Dataset[Crypto], api: String): Dataset[CryptoContainer] = {
-            cryptos.map(crypto =>
-                CryptoContainer(
-                    timestamp = crypto.cryptoValue.datetime.adjusted,
-                    api = api,
-                    partitionKey = crypto.partitionKey,
-                    cryptoValue = crypto.cryptoValue,
-                    tradeMode = crypto.tradeMode,
-                    count = crypto.count
-                ))(CryptoContainer.encoder(spark))
+    private def fill(container: CryptoSpreadContainer): Seq[KrakenCrypto] = {
+        if (container.next == null) Seq(container.toKrakenCrypto) else {
+            val timestamps: Seq[Timestamp] = DateTimeHelper.getTimestamps(container.timestamp, container.next, numberOfMinutesBetweenTwoElement)
+            timestamps.map(ts => container.copy(timestamp = ts).toKrakenCrypto)
         }
-        
+    }
+    
+    private def space(trades: Dataset[Crypto], ohlcs: Dataset[Crypto]): Dataset[KrakenCrypto] = {
+        val spark: SparkSession = trades.sparkSession
+    
         val containersTrades: Dataset[CryptoContainer] = toContainers(trades, "trades")
         val containersOhlcs: Dataset[CryptoContainer] = toContainers(ohlcs, "ohlc")
         
-        val grouped: Dataset[(Timestamp, Seq[CryptoContainer])] = containersTrades
-          .union(containersOhlcs)
-          .withColumn("values", struct("*"))
-          .groupBy("timestamp")
-          .agg(collect_list("values"))
-          .as[(Timestamp, Seq[CryptoContainer])](CryptoContainer.encoderForGroup(spark))
+        val grouped: Dataset[(Timestamp, Seq[CryptoContainer])] =
+            containersTrades
+              .union(containersOhlcs)
+              .withColumn("values", struct("*"))
+              .groupBy("timestamp")
+              .agg(collect_list("values"))
+              .as[(Timestamp, Seq[CryptoContainer])](CryptoContainer.groupByEncoder(spark))
         
         //        grouped.filter(x => x._2.exists(_.api == "ohlc")).take(10).foreach(x => {
         //              val containersTrades: Seq[CryptoContainer] = x._2.filter(_.api == "trades")
@@ -192,4 +139,68 @@ object SpacingSpreadingJoiner {
         spacedKrakenCryptos
     }
     
+    private def toContainers(cryptos: Dataset[Crypto], api: String): Dataset[CryptoContainer] = {
+        cryptos.map(crypto =>
+            CryptoContainer(
+                timestamp = crypto.cryptoValue.datetime.adjusted,
+                api = api,
+                partitionKey = crypto.partitionKey,
+                cryptoValue = crypto.cryptoValue,
+                tradeMode = crypto.tradeMode,
+                count = crypto.count
+            ))(CryptoContainer.encoder(cryptos.sparkSession))
+    }
+    
+    private def oneKrakenCrypto(timestamp: Timestamp, containersTrades: Seq[CryptoContainer], 
+      containersOhlcs: Seq[CryptoContainer]): KrakenCrypto = {
+        
+        if (containersTrades.isEmpty && containersOhlcs.length == 1) {
+            CryptoContainer.fromOHLCToCryptoContainer(timestamp, containersOhlcs.head)
+        } else if (containersTrades.length == 1 && containersOhlcs.isEmpty) {
+            CryptoContainer.fromTRADESToCryptoContainer(timestamp, containersTrades.head)
+        } else if (containersTrades.isEmpty) {
+            val volumeTotal: Double = containersOhlcs.map(_.cryptoValue.volume).sum
+            val countSum: Long = containersOhlcs.map(_.count.getOrElse(0)).sum
+            val countAverage: Int = (countSum / containersOhlcs.length).toInt
+            val cryptoValue: CryptoValue = containersOhlcs.middleCryptoValue
+            
+            KrakenCrypto(
+                datetime = timestamp,
+                value = cryptoValue.value,
+                volume = volumeTotal,
+                count = Option(countAverage),
+                ohlcValue = Option(cryptoValue.value),
+                ohlcVolume = Option(volumeTotal)
+            )
+        } else if (containersOhlcs.isEmpty) {
+            val length = containersTrades.length
+            val volumeTotal: Double = containersTrades.map(_.cryptoValue.volume).sum
+            val cryptoValue: CryptoValue = containersTrades.middleCryptoValue
+            
+            KrakenCrypto(
+                datetime = timestamp,
+                value = cryptoValue.value,
+                volume = volumeTotal,
+                count = None,
+                ohlcValue = None,
+                ohlcVolume = None
+            )
+        } else {
+            val countSum: Long = containersOhlcs.map(_.count.getOrElse(0)).sum
+            val countAverage = (countSum / containersOhlcs.length).toInt
+            val volumeOhlcTotal: Double = containersOhlcs.map(_.cryptoValue.volume).sum
+            val volumeTotal: Double = containersTrades.map(_.cryptoValue.volume).sum + volumeOhlcTotal
+            val cryptoValue: CryptoValue = (containersTrades ++ containersOhlcs).middleCryptoValue
+            val ohlcCryptoValue = containersOhlcs.middleCryptoValue
+            
+            KrakenCrypto(
+                datetime = timestamp,
+                value = cryptoValue.value,
+                volume = volumeTotal,
+                count = Option(countAverage),
+                ohlcValue = Option(ohlcCryptoValue.value),
+                ohlcVolume = Option(volumeTotal)
+            )
+        }
+    }
 }
