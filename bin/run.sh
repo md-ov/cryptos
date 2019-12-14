ddffee08a7833568cd079fcbaee93d45c1a3b3cb
minhov <minhdung.dao@openvalue..fr>
2019-12-14 17:11:33 +0100

    fix automated scripts

--- bin/run.sh
+++ bin/run.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 now="$(date +'%Y-%m-%d')"
-echo "Starting get kraken at $now, please wait..."
 cd /d/ws/cryptos/cryptos/krakenc/
+
 if ! [ -f D:\\ws\\cryptos\\data\\trades\\xbt-before-$now ]; then
-	echo "Folder does not exist"
+	echo "Folder xbt-before-$now does not exist => retrieve trades date from last timestamp"
 	rm ./out/trades/*.csv
+	echo "./out/trades/*.csv removed"
+
 	node main-get-t-a.js \{\"asset\":\"XBT\",\"currency\":\"EUR\",\"n\":1000\}
+	echo "main-get-t-a.js for XBT EUR done"
+
 	mkdir D:\\ws\\cryptos\\data\\trades\\xbt-before-$now\\
+	echo "trades\\xbt-before-$now\\ maked"
+
 	mv -n /d/ws/cryptos/cryptos/krakenc/out/trades/*.csv D:\\ws\\cryptos\\data\\trades\\xbt-before-$now\\
+	echo "krakenc/out/trades/*.csv moved to D:\\ws\\cryptos\\data\\trades\\xbt-before-$now\\"
+	
+	echo "Retrieve trades date from last timestamp done"
+
+	echo "Starting To parquets with spark for xbt-before-$now..."
 	cryptos-apps to-parquets-from-csv --master local --api trades --input-dir D:\\ws\\cryptos\\data\\trades\\xbt-before-$now --parquets-dir file:///D:\\ws\\cryptos\\data\\parquets --minimum 1
+	echo "To parquets with spark for xbt-before-$now done"
 fi
 
+echo "Starting get ohlc and today trades at $now, please wait..."
 rm ./out/ohlc/*.csv
 echo "./out/ohlc/*.csv removed"
 rm ./out/trades/today/*.csv
@@ -37,5 +50,15 @@ rm -r /d/ws/cryptos/data/parquets/XBT/EUR/TRADES/today/*
 echo "parquets/XBT/EUR/TRADES/today/* removed"
 mkdir D:\\ws\\cryptos\\data\\parquets\\XBT\\EUR\\TRADES\\today\\parquet\\
 echo "parquets\\XBT\\EUR\\TRADES\\today\\parquet\\ maked"
+
+echo "Get kraken at $now successfully completed"
+
+echo "Starting To parquets with spark for ohlc xbt..."
+cryptos-apps to-parquets-from-csv --master local --api ohlc --input-dir D:\\ws\\cryptos\\data\\ohlc\\xbt --parquets-dir file:///D:\\ws\\cryptos\\data\\parquets --minimum 500
+echo "To parquets with spark for ohlc xbt done"
+
+echo "Starting To parquets with spark for trades xbt-today-$now..."
+cryptos-apps to-parquets-from-today-csv --master local --api trades --input-dir D:\\ws\\cryptos\\data\\trades\\xbt-today-$now --parquets-dir file:///D:\\ws\\cryptos\\data\\parquets --minimum 1
+echo "To parquets with spark for trades xbt-today-$now done"
 
 cmd
