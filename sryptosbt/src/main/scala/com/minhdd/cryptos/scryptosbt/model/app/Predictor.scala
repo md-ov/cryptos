c5833e9f9f90635cd2d1a71f7ed3d1639f166f51
minhov <minhdung.dao@openvalue..fr>
2020-01-08 18:34:06 +0100

    refactor

--- sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/model/app/Predictor.scala
+++ sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/model/app/Predictor.scala
@@ -6,7 +6,7 @@ import java.util.Date
 import com.minhdd.cryptos.scryptosbt.constants.{directoryNow, numberOfMinutesBetweenTwoElement}
 import com.minhdd.cryptos.scryptosbt.domain.BeforeSplit
 import com.minhdd.cryptos.scryptosbt.env.dataDirectory
-import com.minhdd.cryptos.scryptosbt.model.service.ml.{label, predict, prediction}
+import com.minhdd.cryptos.scryptosbt.model.service.ml.{label, prediction, predict}
 import com.minhdd.cryptos.scryptosbt.segment.app.ActualSegment.getActualSegments
 import com.minhdd.cryptos.scryptosbt.tools.{DateTimeHelper, ModelHelper}
 import org.apache.spark.ml.feature.Binarizer
@@ -37,53 +37,33 @@ object Predictor {
         import spark.implicits._
         val actualSegments: Seq[Seq[BeforeSplit]] = getActualSegments
         val ds: Dataset[Seq[BeforeSplit]] = spark.createDataset(actualSegments).cache()
-        println("number of segments: " + actualSegments.size)
         val lastSegment = ds.collect.last
-        print(DateTimeHelper.defaultDateFormat.format(new Date()))
-        print(";")
-        print(s"$numberOfMinutesBetweenTwoElement/$directoryNow")
-        print(";")
-        print(lastSegment.last.value)
-        print(";")
-        print(lastSegment.last.datetime)
-        print(";")
-        print(lastSegment.head.datetime)
-        print(";")
-        print(lastSegment.head.value)
-        print(";")
-        print(lastSegment.size)
-        print(";")
-        
+        val df = ds.toDF()
+        println("number of segments: " + actualSegments.size)
         val mapBegindtAndSegmentLength: Array[(Timestamp, Int)] = ds.map(x => (x.head.datetime, x.length)).collect()
         //        val segments: DataFrame = Expansion.expansion(spark, ds)
         //        segments.filter(col("numberOfElement") === 2).show(10, false)
-        //        mapBegindtAndSegmentLength.foreach(println)
-        val df = ds.toDF()
         
-        val model: CrossValidatorModel = ModelHelper.getModel(spark, modelPath)
-        val segmentsWithRawPrediction: DataFrame = model.transform(df).cache()
-        val predictionOfLastSegment: DataFrame = segmentsWithRawPrediction
-          .filter(row => !row.getAs[Boolean]("isSegmentEnd") && {
-              val foundElement: Option[(Timestamp, Int)] = mapBegindtAndSegmentLength.find(_._1 == row.getAs[Timestamp]("begindt"))
-              foundElement.get._2 == row.getAs[Int]("numberOfElement")
-          })
-          .select("begindt", "enddt", "isSegmentEnd", "beginEvolution", "endEvolution", "evolutionDirection",
-              "beginvalue", "endvalue", "numberOfElement", "label", "prediction")
+        
+        val (segmentsWithRawPrediction: DataFrame, predictionOfLastSegment: DataFrame) = predictMethod(df, mapBegindtAndSegmentLength)
         val p = predictionOfLastSegment.map(_.getAs[Double]("prediction")).head()
-        print(p)
-        print(";")
-        if (p >= thresholdForPositive) {
-            print(1)
-        } else if (p <= thresholdForNegative) {
-            print(0)
-        } else {
-            print(-1)
-        }
-        println(";;;")
+        
         predictionOfLastSegment.show(false)
         
         println("number of predicted elements: " + segmentsWithRawPrediction.count())
         val targeted: DataFrame = segmentsWithRawPrediction.filter(_.getAs[Boolean]("isSegmentEnd")).cache()
+        
+        val (targetedCount: Long,
+        positiveCount: Long,
+        okPositive: DataFrame,
+        negativeCount: Long,
+        okNegative: DataFrame) = stats(targeted)
+        
+        printPredictionHistory(p, lastSegment)
+        printHistory(actualSegments, lastSegment, targetedCount, positiveCount, okPositive, negativeCount, okNegative)
+    }
+    
+    private def stats(targeted: DataFrame): (Long, Long, DataFrame, Long, DataFrame) = {
         val targetedCount: Long = targeted.count()
         println("number of targeted elements: " + targetedCount)
         val binarizerForSegmentDetection = new Binarizer()
@@ -104,7 +84,24 @@ object Predictor {
         val okNegative: DataFrame = negative.filter(col(predict) === col(label))
         println("negative rate: " + okNegative.count() + "/" + negativeCount + " - " +
           (negativeCount.toDouble / targetedCount))
+        
+        (targetedCount, positiveCount, okPositive, negativeCount, okNegative)
+    }
     
+    private def predictMethod(df: DataFrame, mapBegindtAndSegmentLength: Array[(Timestamp, Int)] ) = {
+        val model: CrossValidatorModel = ModelHelper.getModel(spark, modelPath)
+        val segmentsWithRawPrediction: DataFrame = model.transform(df).cache()
+        val predictionOfLastSegment: DataFrame = segmentsWithRawPrediction
+          .filter(row => !row.getAs[Boolean]("isSegmentEnd") && {
+              val foundElement: Option[(Timestamp, Int)] = mapBegindtAndSegmentLength.find(_._1 == row.getAs[Timestamp]("begindt"))
+              foundElement.get._2 == row.getAs[Int]("numberOfElement")
+          })
+          .select("begindt", "enddt", "isSegmentEnd", "beginEvolution", "endEvolution", "evolutionDirection",
+              "beginvalue", "endvalue", "numberOfElement", "label", "prediction")
+        (segmentsWithRawPrediction, predictionOfLastSegment)
+    }
+    
+    private def printHistory(actualSegments: Seq[Seq[BeforeSplit]], lastSegment: Seq[BeforeSplit], targetedCount: Long, positiveCount: Long, okPositive: DataFrame, negativeCount: Long, okNegative: DataFrame) = {
         print(DateTimeHelper.defaultDateFormat.format(new Date()))
         print(";")
         print(s"$numberOfMinutesBetweenTwoElement/$directoryNow")
@@ -124,5 +121,32 @@ object Predictor {
         print(lastSegment.last.datetime)
         print(";")
         print(actualSegments.size)
+    }
+    
+    private def printPredictionHistory(p: Double, lastSegment: Seq[BeforeSplit]) = {
+        print(DateTimeHelper.defaultDateFormat.format(new Date()))
+        print(";")
+        print(s"$numberOfMinutesBetweenTwoElement/$directoryNow")
+        print(";")
+        print(lastSegment.last.value)
+        print(";")
+        print(lastSegment.last.datetime)
+        print(";")
+        print(lastSegment.head.datetime)
+        print(";")
+        print(lastSegment.head.value)
+        print(";")
+        print(lastSegment.size)
+        print(";")
+        print(p)
+        print(";")
+        if (p >= thresholdForPositive) {
+            print(1)
+        } else if (p <= thresholdForNegative) {
+            print(0)
+        } else {
+            print(-1)
+        }
+        println(";;;")
     }
 }
