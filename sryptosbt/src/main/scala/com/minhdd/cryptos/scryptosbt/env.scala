636f99ca7377624ebc391d8596dbc8df25e579e8
minhdungdao <md.insa@gmail.com>
2020-02-26 11:02:09 +0100

    env for mac and dell

--- sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/env.scala
+++ sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/env.scala
@@ -1,9 +1,21 @@
 package com.minhdd.cryptos.scryptosbt
 
 object env {
-    
-    val dataDirectory = "C:\\ws\\ov\\data\\cryptos"
-    val dataDirectoryy = "C://ws//ov//data//cryptos"
-    val parquetPath = s"$dataDirectoryy//parquets"
-    val todayPath = s"$dataDirectoryy//parquets//XBT//EUR//TRADES//today//parquet"
+    //Windows
+    //val env = "win"
+    //val prefixPath = "file:///"
+    //val pathDelimiter = "//"
+    //val dataDirectory = "C:\\ws\\ov\\cryptos\\data"
+    //val dataDirectoryy = "C://ws//ov//cryptos//data"
+    //val parquetPath = s"$dataDirectoryy\\parquets"
+    //val todayPath = s"$dataDirectoryy//parquets//XBT//EUR//TRADES//today//parquet"
+
+
+    //MAC
+    val env = "mac"
+    val prefixPath = ""
+    val pathDelimiter = "/"
+    val dataDirectory = "/Users/minhdungdao/ws/data/cryptos"
+    val parquetPath = s"${dataDirectory}/parquets"
+    val todayPath = s"${dataDirectory}/parquets/XBT/EUR/TRADES/today/parquet"
 }
