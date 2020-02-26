a9c615f4ee8ded07e5383fcad7cd34c8263b34fc
minhdungdao <md.insa@gmail.com>
2020-02-26 15:55:33 +0100

    intelligent env

--- sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/env.scala
+++ sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/env.scala
@@ -1,22 +1,31 @@
 package com.minhdd.cryptos.scryptosbt
 
 object env {
-    val pathDelimiter = "/"
-    
-    //Windows
+  val pathDelimiter = "/"
+
+  val props = mac
+
+  val env = props.env
+  val prefixPath = props.prefixPath
+  val dataDirectory = props.dataDirectory
+  val parquetsPath = props.parquetsPath
+  val todayPath = props.todayPath
+
+  object mac {
+    val env = "mac"
+    val prefixPath = ""
+    val dataDirectory = "/Users/minhdungdao/ws/data/cryptos"
+    val parquetsPath = s"${dataDirectory}/parquets"
+    val todayPath = s"${dataDirectory}/parquets/XBT/EUR/TRADES/today/parquet"
+  }
+
+  object dell {
     val env = "win"
     val prefixPath = "file:///"
     val dataDirectory = "C:/ws/ov/cryptos/data"
     val dataDirectoryy = "C:/ws/ov/cryptos/data"
-//    val parquetPath = s"$dataDirectoryy\\parquets"
+    //    val parquetPath = s"$dataDirectoryy\\parquets"
     val parquetsPath = s"$dataDirectoryy${pathDelimiter}parquets"
     val todayPath = s"$dataDirectoryy/parquets/XBT/EUR/TRADES/today/parquet"
-    
-    //MAC
-    //val env = "mac"
-    //val prefixPath = ""
-    //val pathDelimiter = "/"
-    //val dataDirectory = "/Users/minhdungdao/ws/data/cryptos"
-    //val parquetsPath = s"${dataDirectory}/parquets"
-    //val todayPath = s"${dataDirectory}/parquets/XBT/EUR/TRADES/today/parquet"
+  }
 }
