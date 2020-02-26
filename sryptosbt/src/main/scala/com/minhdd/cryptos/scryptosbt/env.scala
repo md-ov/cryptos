725a26aeb58978ab9d3b0bba78a0cc2987a8f0c8
Minh Dung DAO <mddao@tf1.fr>
2020-02-26 15:46:42 +0100

    dell env

--- sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/env.scala
+++ sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/env.scala
@@ -1,21 +1,22 @@
 package com.minhdd.cryptos.scryptosbt
 
 object env {
-    //Windows
-    //val env = "win"
-    //val prefixPath = "file:///"
-    //val pathDelimiter = "//"
-    //val dataDirectory = "C:\\ws\\ov\\cryptos\\data"
-    //val dataDirectoryy = "C://ws//ov//cryptos//data"
-    //val parquetPath = s"$dataDirectoryy\\parquets"
-    //val todayPath = s"$dataDirectoryy//parquets//XBT//EUR//TRADES//today//parquet"
-
-
-    //MAC
-    val env = "mac"
-    val prefixPath = ""
     val pathDelimiter = "/"
-    val dataDirectory = "/Users/minhdungdao/ws/data/cryptos"
-    val parquetsPath = s"${dataDirectory}/parquets"
-    val todayPath = s"${dataDirectory}/parquets/XBT/EUR/TRADES/today/parquet"
+    
+    //Windows
+    val env = "win"
+    val prefixPath = "file:///"
+    val dataDirectory = "C:/ws/ov/cryptos/data"
+    val dataDirectoryy = "C:/ws/ov/cryptos/data"
+//    val parquetPath = s"$dataDirectoryy\\parquets"
+    val parquetsPath = s"$dataDirectoryy${pathDelimiter}parquets"
+    val todayPath = s"$dataDirectoryy/parquets/XBT/EUR/TRADES/today/parquet"
+    
+    //MAC
+    //val env = "mac"
+    //val prefixPath = ""
+    //val pathDelimiter = "/"
+    //val dataDirectory = "/Users/minhdungdao/ws/data/cryptos"
+    //val parquetsPath = s"${dataDirectory}/parquets"
+    //val todayPath = s"${dataDirectory}/parquets/XBT/EUR/TRADES/today/parquet"
 }
