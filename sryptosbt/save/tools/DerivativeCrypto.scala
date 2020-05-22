4f0e85aa27d49c70163eef7606414a4b254b3650
minhdungdao <md.insa@gmail.com>
2020-05-22 23:59:33 +0200

    move

--- sryptosbt/save/tools/DerivativeCrypto.scala
+++ sryptosbt/save/tools/DerivativeCrypto.scala
@@ -4,18 +4,19 @@ import com.minhdd.cryptos.scryptosbt.domain.Crypto
 import org.apache.spark.sql.{Dataset, Encoder, SparkSession}
 
 object DerivativeCrypto {
-    def toX(c: Crypto) = c.cryptoValue.datetime.getTime.toDouble / 1000000
-    def toY(c: Crypto) = c.cryptoValue.value
-    def encoder(ss: SparkSession): Encoder[(Crypto, Double)] = {
-        import ss.implicits._
-        implicitly[Encoder[(Crypto, Double)]]
-    }
-     
-    def derive(d: Dataset[Crypto], ss: SparkSession): Dataset[(Crypto, Double)] =
-        d.mapPartitions(iterator => new Iterator[(Crypto, Double)]{
+    def toX(c: Crypto): Double = c.cryptoValue.datetime.getTime.toDouble / 1000000
+
+    def toY(c: Crypto): Double = c.cryptoValue.value
+
+    def derive(d: Dataset[Crypto], spark: SparkSession): Dataset[(Crypto, Double)] = {
+        import spark.implicits._
+
+        d.mapPartitions(iterator => new Iterator[(Crypto, Double)] {
             var previous: Option[Crypto] = None
             var actual: Option[Crypto] = if (iterator.hasNext) Some(iterator.next) else None
+
             override def hasNext: Boolean = actual.isDefined
+
             override def next(): (Crypto, Double) = {
                 val actualGet: Crypto = actual.get
                 val nextt: Option[Crypto] = if (iterator.hasNext) Some(iterator.next) else None
@@ -25,13 +26,13 @@ object DerivativeCrypto {
                             val nextGet = nextt.get
                             previous = actual
                             actual = nextt
-                            (toY(nextGet) - toY(actualGet))/(toX(nextGet) - toX(actualGet)) - ( toX(nextGet) - toX(actualGet) )
+                            (toY(nextGet) - toY(actualGet)) / (toX(nextGet) - toX(actualGet)) - (toX(nextGet) - toX(actualGet))
                         }
                     } else if (nextt.isEmpty) {
                         val previousGet = previous.get
                         previous = actual
                         actual = nextt
-                        (toY(actualGet) - toY(previousGet))/(toX(actualGet) - toX(previousGet)) + (toX(actualGet) - toX(previousGet))
+                        (toY(actualGet) - toY(previousGet)) / (toX(actualGet) - toX(previousGet)) + (toX(actualGet) - toX(previousGet))
                     } else {
                         val nextGet = nextt.get
                         val previousGet = previous.get
@@ -39,8 +40,9 @@ object DerivativeCrypto {
                         actual = nextt
                         (toY(nextGet) - toY(previousGet)) / (toX(nextGet) - toX(previousGet))
                     }
-                
+
                 (actualGet, derived)
             }
-        })(encoder(ss))
+        })
+    }
 }
