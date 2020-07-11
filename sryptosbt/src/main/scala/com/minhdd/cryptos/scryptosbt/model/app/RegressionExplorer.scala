66d42ce99784920b97fd5e129ad25a9bb6767d5d
minhdungdao <md.insa@gmail.com>
2020-07-11 11:34:25 +0200

    updates

--- sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/model/app/RegressionExplorer.scala
+++ sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/model/app/RegressionExplorer.scala
@@ -2,7 +2,7 @@ package com.minhdd.cryptos.scryptosbt.model.app
 
 import com.minhdd.cryptos.scryptosbt.env.dataDirectory
 import com.minhdd.cryptos.scryptosbt.model.service.ml._
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 import org.apache.spark.sql.functions.{avg, col}
 
 //après regression trainer
@@ -23,25 +23,24 @@ object RegressionExplorer {
 //      .show(false)
 
 
-    df.map(x => {
+    val df1: Dataset[(Double, Double, Double)] = df.map(x => {
 //      val label = x.getAs[Double]("label").toDouble
             val label = x.getAs[Int]("label").toDouble
       val prediction = x.getAs[Double]("prediction")
       val error = (prediction - label)/label
       (label, prediction, scala.math.abs(error))
-    }).sort("_3")
+    })
+      .filter(col("_2") < 100)
+
+    df1.sort("_3")
       .show(10, false)
 
-    val errorDf = df.map(x => {
-//      val label = x.getAs[Double]("label").toDouble
-      val label = x.getAs[Int]("label").toDouble
-      val prediction = x.getAs[Double]("prediction")
-      val error = (prediction - label)/label
-      scala.math.abs(error)
+    val errorDf = df1.map(x => {
+      scala.math.abs(x._3)
     })
 
     println(errorDf.count())
-    println(errorDf.filter(col("value") < 0.5).count())
+    println(errorDf.filter(col("value") < 0.4).count())
 
     errorDf.agg(avg("value")).show()
   }
