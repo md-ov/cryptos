bd9530d973ca6d842794f2dbf485bd9cee0aa267
minhdungdao <md.insa@gmail.com>
2020-11-22 10:26:33 +0100

    updates

--- sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/model/app/ResultTaker.scala
+++ sryptosbt/src/main/scala/com/minhdd/cryptos/scryptosbt/model/app/ResultTaker.scala
@@ -14,7 +14,7 @@ import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
 //3 après predictor
 object ResultTaker {
     def main(args: Array[String]): Unit = {
-        main("2020-11-17 18:45:00")
+        main("2020-11-21 03:15:00")
     }
     
     val spark: SparkSession = SparkSession.builder()
